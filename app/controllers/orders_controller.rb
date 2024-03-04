a3d09e503e648d84d16de0a12ac1a26c7b34e361
alma-hernandez <1984ahern@gmail.com>
2024-03-04 14:14:27 -0600

    begin controller logic, established routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,30 @@
-class OrdersController < ApplicationController
-end
+class OrderssController < ApplicationController
+    def index
+      # Logic to fetch all orders and render index view
+    end
+  
+    def show
+      # Logic to fetch a specific orders and render show view
+    end
+  
+    def new
+      # Logic to initialize a new order and render new view
+    end
+  
+    def create
+      # Logic to save a new order and redirect to index or show view
+    end
+  
+    def edit
+      # Logic to fetch a order for editing and render edit view
+    end
+  
+    def update
+      # Logic to update an existing order and redirect to index or show view
+    end
+  
+    def destroy
+      # Logic to delete a order and redirect to index view
+    end
+  end
+  
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,30 @@
 class ProductsController < ApplicationController
-end
+    def index
+      # Logic to fetch all products and render index view
+    end
+  
+    def show
+      # Logic to fetch a specific product and render show view
+    end
+  
+    def new
+      # Logic to initialize a new product and render new view
+    end
+  
+    def create
+      # Logic to save a new product and redirect to index or show view
+    end
+  
+    def edit
+      # Logic to fetch a product for editing and render edit view
+    end
+  
+    def update
+      # Logic to update an existing product and redirect to index or show view
+    end
+  
+    def destroy
+      # Logic to delete a product and redirect to index view
+    end
+  end
+  
